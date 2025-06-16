39df13c304a7b068804a9ee701c7f103600ae1bc
Nazareth2710 <w.nazar2003@gmail.com>
2025-06-16 23:54:20 +0300

    Added buttons for fast switching to prediction pages

--- pages/home.py
+++ pages/home.py
@@ -7,3 +7,12 @@ st.image("images/lung-cancer-2.png")
 st.title(t["home_page"]["title"])
 st.write(t["home_page"]["description"])
 
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button(t["home_page"]["go_to_image"], use_container_width=True):
+        st.switch_page("pages/image_prediction.py")
+
+with col2:
+    if st.button(t["home_page"]["go_to_form"], use_container_width=True):
+        st.switch_page("pages/form_prediction.py")
